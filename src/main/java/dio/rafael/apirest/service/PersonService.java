befa2d512148fb03f15667c0718a5dd850d49e7a
rafaelmocorrea <rafaelmocorrea@gmail.com>
2021-08-30 23:44:17 -0300

    update adicionado

--- src/main/java/dio/rafael/apirest/service/PersonService.java
+++ src/main/java/dio/rafael/apirest/service/PersonService.java
@@ -28,10 +28,7 @@ public class PersonService {
         Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Pessoa criada com ID "+savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Pessoa criada com ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -51,8 +48,25 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+
+        verifyIfExists(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(updatedPerson.getId(), "Pessoa atualizada com ID ");
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
     }
 }
