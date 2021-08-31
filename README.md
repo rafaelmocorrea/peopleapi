8a90536cbff9ba49e6f1a48c53de073d9ae0bc78
rafaelmocorrea <rafaelmocorrea@gmail.com>
2021-08-31 11:10:40 -0300

    readme adicionado

---
+++ README.md
@@ -0,0 +1,17 @@
+###API de gerenciamento de pessoas implementado no Santander Bootcamp | Fullstack Developer na Digital Innovation One
+
+API com métodos CRUD desenvolvida em Java 11 com o uso de SpringBoot, Lombok e H2.
+
+A classe *PersonService* apresenta os seguintes métodos públicos:
+
+* createPerson(): insere uma pessoa no BD.
+* listAll(): retorna uma lista com todas as pessoas no BD.
+* findById(): retorna a pessoa com o ID informado.
+* deleteById(): apaga a pessoa com o ID informado.
+* updateById(): atualiza os dados da pessoa com o ID informado.
+
+Os métodos privados são:
+
+* verifyIfExists(): verifica se a pessoa de ID informado existe no BD.
+* createMessageResponse(): cria uma mensagem de resposta da API.
+
--- src/main/java/dio/rafael/apirest/controller/PersonController.java
+++ src/main/java/dio/rafael/apirest/controller/PersonController.java
@@ -5,6 +5,7 @@ import dio.rafael.apirest.dto.response.MessageResponseDTO;
 import dio.rafael.apirest.entity.Person;
 import dio.rafael.apirest.exception.PersonNotFoundException;
 import dio.rafael.apirest.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +15,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
